51bfe8771a8965c444ee2d4f0b8c69923d40b88b
Gal Ben-Haim <gbenhaim@augury.com>
2014-11-12 12:09:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+nsqworker - an async task worker for NSQ
+----------------------------------------
+
+Currently only a threaded worker is supported.
+
+Usage:
+```
+import time
+import nsqworker
+
+
+def process_message(message):
+  print "start", message.id
+  time.sleep(2)
+  print "end", message.id
+
+def handle_exc(message, e):
+  traceback.print_exc()
+  message.requeue()
+
+w.nsqworker.ThreadWorker(process_message, exception_handler=handle_exc, concurrency=5, ...)
+
+w.start()
+```
+
+The arguments for the `ThreadWorker` constructor are a synchronous, blocking function that handles messages, concurrency, an optional exception_handler and all other arguments for the official [NSQ](http://nsq.io) Python library - [pynsq](https://pynsq.readthedocs.org).
+
+* The worker will explicitly call `message.finish()` in case the handler function didn't call `message.finish()` or `message.requeue()`.
+
+* The worker will periodically call `message.touch()` every 30s for long running tasks so they won't timeout by nsqd.
+
+* The exception handler is called with a message and an exception as the arguments in case it was given during the worker's initialization and an exception is raised while processing a message.
+
+* TODO - add definable timeout for message handling.
---
+++ nsqworker/nsqworker.py
@@ -0,0 +1,71 @@
+import logging
+import sys
+
+import nsq
+from tornado import gen
+from tornado import ioloop
+from concurrent.futures import ThreadPoolExecutor
+from tornado.concurrent import run_on_executor
+
+
+class ThreadWorker:
+    def __init__(self, message_handler, exception_handler=None, concurrency=1, timeout=60, **kwargs):
+        self.io_loop = ioloop.IOLoop.instance()
+        self.executor = ThreadPoolExecutor(concurrency)
+        self.concurrency = concurrency
+        self.kwargs = kwargs
+        self.message_handler = message_handler
+        self.exception_handler = exception_handler
+        self.timeout = timeout
+
+        self.logger = logging.getLogger("ThreadWorker")
+        if not self.logger.handlers:
+            formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+            handler = logging.StreamHandler(stream=sys.stdout)
+            handler.setFormatter(formatter)
+            handler.setLevel(logging.DEBUG)
+
+            self.logger.addHandler(handler)
+            self.logger.setLevel(logging.DEBUG)
+            self.logger.propagate = 0
+
+    @run_on_executor
+    def _run_threaded_handler(self, message):
+        self.message_handler(message)
+
+    @gen.coroutine
+    def _message_handler(self, message):
+        self.logger.debug("Received message %s", message.id)
+        message.enable_async()
+
+        def touch():
+            self.logger.debug("Sending touch event for message %s", message.id)
+            message.touch()
+
+        p = ioloop.PeriodicCallback(touch, 30000)
+        p.start()
+
+        try:
+            result = yield self._run_threaded_handler(message)
+        except Exception as e:
+            self.logger.debug("Message handler for message %s raised an exception", message.id)
+            if self.exception_handler is not None:
+                self.exception_handler(message, e)
+
+        p.stop()
+
+        if not message.has_responded():
+            message.finish()
+
+        self.logger.debug("Finished handling message %s", message.id)
+
+    def start(self):
+        self.logger.info("Listening for NSQD messages, handling message with %d threads", self.concurrency)
+        kwargs = self.kwargs
+        kwargs["message_handler"] = self._message_handler
+        kwargs["max_in_flight"] = self.concurrency
+
+        nsq.Reader(**kwargs)
+
+        nsq.run()
